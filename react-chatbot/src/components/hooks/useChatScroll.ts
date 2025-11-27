be59b1dcb06c01a3d2dee0607f3107e90a2653da
Sravanth Ch <sravanthch18@gmail.com>
2025-11-27 15:19:32 +0530

    Handle Scroller with Custom Hook

---
+++ react-chatbot/src/components/hooks/useChatScroll.ts
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef } from 'react'
+
+function useChatScroll<T>(dep: T):React.RefObject<HTMLDivElement | null>  {
+    const ref = useRef<HTMLDivElement | null>(null)
+    useEffect(()=>{
+        if(ref.current) {
+            ref.current.scrollTop = ref.current.scrollHeight;
+        }
+    },[dep])
+  return ref;
+}
+
+export default useChatScroll
